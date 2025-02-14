b9a05dcf84084729a6fd26ccc8b4ef8070f848d9
Shdhejsnn <hmskanda06@gmail.com>
2025-02-14 19:44:42 +0530

    Temporary commit before rebase

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,16 @@
+// firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-sTOxpppPEsqtgfs1OAd_Q6BCLxWb2e8",
+    authDomain: "green-ledger.firebaseapp.com",
+    projectId: "green-ledger",
+    storageBucket: "green-ledger.firebasestorage.app",
+    messagingSenderId: "1075843157478",
+    appId: "1:1075843157478:web:8687c7e79a337ea5571a27",
+    measurementId: "G-1XGSXEKFVR"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
